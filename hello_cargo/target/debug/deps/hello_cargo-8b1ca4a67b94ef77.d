28c0e503a3428cb7da27997f5a70b7db97401439
GecklesTheClown <nicholas.gecks@proton.me>
2026-08-17 22:53:44 +1000

    Remove duplicated Ch01 projects from repo root

--- hello_cargo/target/debug/deps/hello_cargo-8b1ca4a67b94ef77.d
+++
@@ -1,5 +0,0 @@
-/home/ngecks/Projects/rusty-spoon/hello_cargo/target/debug/deps/hello_cargo-8b1ca4a67b94ef77.d: src/main.rs
-
-/home/ngecks/Projects/rusty-spoon/hello_cargo/target/debug/deps/libhello_cargo-8b1ca4a67b94ef77.rmeta: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/ngecks/Projects/rusty-spoon/hello_cargo/target/debug/hello_cargo: /home/ngecks/Projects/rusty-spoon/hello_cargo/src/main.rs
